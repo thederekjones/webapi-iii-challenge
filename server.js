a719f17ee8fc43ea160b938551f2ad7a8ca71a4b
Derek Jones <derek@derekjones.io>
2019-07-10 23:22:18 -0400

    Set up server with routes to posts and users

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
-const express = 'express';
+const express = require('express');
+
+const postRouter = require('./posts/postRouter');
+const userRouter = require('./users/userRouter');
 
 const server = express();
 
+server.use(express.json());
+server.use('/api/posts', postRouter);
+server.use('/api/users', userRouter);
+
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+  res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {
-
-};
+function logger(req, res, next) {}
 
 module.exports = server;
